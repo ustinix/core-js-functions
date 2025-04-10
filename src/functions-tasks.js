74d902991e55e04c46e20c6fbe9e3d3dbfcce523
Ustinix <ksuletters@yandex.ru>
2025-04-10 19:44:54 +0300

    par2

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -198,8 +198,10 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args2) => {
+    return fn(...args1, ...args2);
+  };
 }
 
 /**
@@ -219,8 +221,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let id = startFrom;
+
+  return function counter() {
+    id += 1;
+    return id - 1;
+  };
 }
 
 module.exports = {
